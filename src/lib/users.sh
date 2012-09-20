bd75a2766e215fa114d219bb1abaa98c3def6bb3
Lance Lovette <lance.lovette@gmail.com>
2012-09-20 10:58:46 -0400

    Fix incorrect option to useradd.

--- src/lib/users.sh
+++ src/lib/users.sh
@@ -61,7 +61,7 @@ function bootstrap_user_add_system()
 
 	if ! bootstrap_user_exists $UNAME; then
 		bootstrap_user_group_add $ADDUID $UNAME
-		/usr/sbin/useradd -M -r -n -u $ADDUID -g $UNAME -c "$UCOMMENT" -d "$UHOME" -s /sbin/nologin $UNAME
+		/usr/sbin/useradd -M -r -N -u $ADDUID -g $UNAME -c "$UCOMMENT" -d "$UHOME" -s /sbin/nologin $UNAME
 		[ $? -ne 0 ] && bootstrap_die
 		echo " * created user ${UNAME} (no login)"
 	fi
@@ -86,7 +86,7 @@ function bootstrap_user_add_login()
 
 	if ! bootstrap_user_exists $UNAME; then
 		bootstrap_user_group_add $ADDUID $UNAME
-		/usr/sbin/useradd -u $ADDUID -g $UNAME -c "$UCOMMENT" -m -n -s /bin/bash -G users -p $UPASSCRYPT $UNAME
+		/usr/sbin/useradd -u $ADDUID -g $UNAME -c "$UCOMMENT" -m -N -s /bin/bash -G users -p $UPASSCRYPT $UNAME
 		[ $? -ne 0 ] && bootstrap_die
 		# Force password change on next login
 		/usr/bin/chage -d 0 $UNAME
